a12e911ae84dce5ef757989a4c2dbddba4c5fb02
Ying <ying-choi@users.noreply.github.com>
2017-03-19 02:47:53 -0700

    Add chart in main activity

--- app/src/main/java/com/nut/nwhacks/main/MainActivity.java
+++ app/src/main/java/com/nut/nwhacks/main/MainActivity.java
@@ -3,6 +3,7 @@ package com.nut.nwhacks.main;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +13,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.db.chart.animation.Animation;
+import com.db.chart.model.LineSet;
+import com.db.chart.view.LineChartView;
 import com.nut.nwhacks.R;
 import com.nut.nwhacks.logtrip.TagListAdapter;
 import com.nut.nwhacks.settings.AddTagActivity;
@@ -32,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
     private static List<String> sTagList;
     private static TagListAdapter sAdapter;
     private int mNumTags;
+    private LineChartView mLineChartView;
+    private ListView mListView;
+    private String[] mLabels;
+    private Float[] mValues;
+    private LineSet mDataSet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +52,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ListView listView = (ListView)findViewById(R.id.main_listview);
-        sAdapter = new TagListAdapter(this, R.layout.itemlistrow, sTagList);
-        listView.setAdapter(sAdapter);
+        getScores();
+        initializeViews();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setVisibility(View.VISIBLE);
@@ -66,6 +75,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void initializeViews() {
+        mLineChartView = (LineChartView) findViewById(R.id.lineChart);
+        drawChart();
+
+        mListView = (ListView)findViewById(R.id.main_listview);
+        sAdapter = new TagListAdapter(this, R.layout.itemlistrow, sTagList);
+        mListView.setAdapter(sAdapter);
     }
 
     private void getTagsFromPreferences() {
@@ -90,4 +108,49 @@ public class MainActivity extends AppCompatActivity {
         sAdapter.notifyDataSetChanged();
     }
 
+    public void drawChart() {
+        Animation anim = new Animation(3000);
+        mLineChartView.addData(mDataSet);
+        mLineChartView.show(anim);
+    }
+
+    public void getScores() {
+        // TODO: GET THE SCORES IN STRING[] (dates) AND FLOAT[] FORMAT
+        // Placeholder Data
+        List<String> labels = new ArrayList<>();
+        labels.add("First");
+        labels.add("Second");
+        labels.add("Third");
+        labels.add("Fourth");
+        labels.add("Fifth");
+
+        mLabels = new String[labels.size()];
+        labels.toArray(mLabels);
+
+        List<Float> values = new ArrayList<>();
+        values.add(39f);
+        values.add(20f);
+        values.add(60f);
+        values.add(90f);
+        values.add(24f);
+
+        mValues = new Float[values.size()];
+        values.toArray(mValues);
+        addEntries();
+    }
+
+    private void addEntries() {
+        mDataSet = new LineSet();
+        mDataSet.setColor(Color.parseColor("#0080ff"))
+                .setFill(Color.parseColor("#0080ff"))
+                .setSmooth(true);
+
+        if (mLabels.length == mValues.length) {
+            for (int i = 0; i < mValues.length; i++) {
+                mDataSet.addPoint(mLabels[i], mValues[i]);
+            }
+        } else {
+            System.out.println("the labels and values aren't the same length");
+        }
+    }
 }
